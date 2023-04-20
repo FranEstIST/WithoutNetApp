be7f9d4e88a39a79799afb076eb9ddecb01e3f19
Francisco <francisco.ferreira.esteves@tecnico.ulisboa.pt>
2023-04-20 20:57:56 +0100

    Added a class to handle the recognition of reading types

---
+++ app/src/main/java/pt/ulisboa/tecnico/withoutnet/ReadingTypeIDs.java
@@ -0,0 +1,16 @@
+package pt.ulisboa.tecnico.withoutnet;
+
+public class ReadingTypeIDs {
+    public static final String BATTERY_READING_ID = "2A19";
+
+    public static String getReadingTypeName(String id) {
+        switch(id) {
+            case BATTERY_READING_ID:
+                // TODO: Implement the global context so that this method
+                // can return a string defined in the resources
+                return "Battery Reading";
+            default:
+                return "Unknown Type";
+        }
+    }
+}
