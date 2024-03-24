8801dce0154d16020295ab282d5c20c654bc238f
Francisco <francisco.ferreira.esteves@tecnico.ulisboa.pt>
2024-03-24 21:56:44 +0000

    Implemented the basic UI for viewing nodes in a  network and viewing a node's characteristics

---
+++ app/src/main/java/pt/ulisboa/tecnico/withoutnet/activities/NodeDetailsActivity.java
@@ -0,0 +1,16 @@
+package pt.ulisboa.tecnico.withoutnet.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ulisboa.tecnico.withoutnet.R;
+
+public class NodeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_node_details);
+    }
+}
---
+++ app/src/main/java/pt/ulisboa/tecnico/withoutnet/activities/NodesListActivity.java
@@ -0,0 +1,16 @@
+package pt.ulisboa.tecnico.withoutnet.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ulisboa.tecnico.withoutnet.R;
+
+public class NodesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nodes_list);
+    }
+}
